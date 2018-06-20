da96ec3739c02dbeb4be422b6fe051c400eb81da
Raghav Prakash <Raghav-Prakash@users.noreply.github.com>
2018-06-19 21:31:34 -0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,13 +5,12 @@ $(document).ready( function(){
 
         var isOkay = check(email,password);
         if(!isOkay) {
-            alert('Email or password cannot be blank');
             return;
         }
 
         $.ajax(
             {
-                url: "/php/login.php",
+                url: "/WMIS/php/login.php",
                 type: "POST",
                 data: {email:email, password:password},
                 success: function(msg) {
@@ -27,8 +26,28 @@ $(document).ready( function(){
     });
 });
 function check(email,password) {
-    if(email == '' || password == '') {
-        return false;
+    var email_ok = false;
+    var password_ok = false;
+
+    var email_addr=email.toLowerCase();
+    var email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   
+    if(email_re.test(email_addr)==false){
+        $('#email').tooltip({trigger: 'manual'}).tooltip('show');
+        email_ok = false;
     }
-    return true;
+    else{
+        $('#email').tooltip('hide');
+        email_ok = true;
+    }
+    if(password.replace(/[ ]/g,"")==""){
+        $('#password').tooltip({trigger: 'manual'}).tooltip('show');
+        password_ok = false;
+    }
+    else{
+        $('#password').tooltip('hide');
+        password_ok = true;
+    }
+
+    return email_ok && password_ok;
 }
